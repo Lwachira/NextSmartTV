59219690f3722b8ac7cd0c7a89ba38342735e979
Lincoln Gachagua <blazeboy.wachira@gmail.com>
2021-05-15 11:46:58 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
 import Menu from "../Components/Menu";
 import PhotoGrid from "../Components/PhotoGrid";
 
 export default function Home() {
-  const [topicname, setTopicName] = useState("");
+  const [topicname, setTopicName] = useState("film");
   console.log(topicname);
   return (
     <Box>
@@ -24,6 +24,7 @@ export default function Home() {
         <Menu setTopicName={setTopicName}></Menu>
         <PhotoGrid topicname={topicname}></PhotoGrid>
       </Flex>
+
     </Box>
   );
 }
